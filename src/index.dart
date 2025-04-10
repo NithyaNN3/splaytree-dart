0d651e1bf766b9277e59380ff1e9b0f55a325c54
NithyaNN3 <nithyaniranjani@gmail.com>
2025-04-10 23:47:33 +1000

    added splaymax, splaymin and splay

--- src/index.dart
+++ src/index.dart
@@ -27,3 +27,145 @@ class SplayTreeMapNode<K, V> extends SplayTreeNode<K, SplayTreeMapNode<K, V>> {
     }
 
 }
+
+abstract class SplayTree<K, Node extends SplayTreeNode<K, Node>> {
+    Node? root;
+
+    int size = 0;
+
+    int modificationCount = 0;
+
+    int splayCount = 0;
+
+    Comparator<K> get compare;
+
+    Predicate<Object>> get validKey;
+
+    int splay(K key) {
+        final root = this.root;
+        if (root == null) {
+            this.compare(key, key);
+            return -1;
+        }
+        
+        Node? right = null;
+        Node? newTreeRight = null;
+        Node? left = null;
+        Node? newTreeLeft = null;
+        Node current = root;
+        final compare = this.compare;
+        int comp;
+        
+        while (true) {
+            comp = compare(current.key, key);
+            if (comp > 0) {
+            Node? currentLeft = current.left;
+            if (currentLeft == null) break;
+            comp = compare(currentLeft.key, key);
+            if (comp > 0) {
+                current.left = currentLeft.right;
+                currentLeft.right = current;
+                current = currentLeft;
+                currentLeft = current.left;
+                if (currentLeft == null) break;
+            }
+            if (right == null) {
+                newTreeRight = current;
+            } else {
+                right.left = current;
+            }
+            right = current;
+            current = currentLeft!;
+            } else if (comp < 0) {
+            Node? currentRight = current.right;
+            if (currentRight == null) break;
+            comp = compare(currentRight.key, key);
+            if (comp < 0) {
+                current.right = currentRight.left;
+                currentRight.left = current;
+                current = currentRight;
+                currentRight = current.right;
+                if (currentRight == null) break;
+            }
+            if (left == null) {
+                newTreeLeft = current;
+            } else {
+                left.right = current;
+            }
+            left = current;
+            current = currentRight!;
+            } else {
+            break;
+            }
+        }
+        
+        if (left != null) {
+            left.right = current.left;
+            current.left = newTreeLeft;
+        }
+        if (right != null) {
+            right.left = current.right;
+            current.right = newTreeRight;
+        }
+        
+        if (this.root != current) {
+            this.root = current;
+            this.splayCount++;
+        }
+        
+        return comp;
+    }
+
+    Node splayMin(Node node) {
+        Node current = node;
+        Node? nextLeft = current.left;
+        while (nextLeft != null) {
+            final left = nextLeft;
+            current.left = left.right;
+            left.right = current;
+            current = left;
+            nextLeft = current.left;
+        }
+        return current;
+    }
+
+    Node splayMax(Node: node) {
+        Node current = node;
+        Node? nextRight = current.right;
+        while (nextRight != null) {
+            final right = nextRight;
+            current.right = right.left;
+            right.left = current;
+            current = right;
+            nextRight = current.right;
+        }
+        return current;
+    }
+
+    Node? _delete(K key) {
+        if (this.root == null) return null;
+        final comp = this.splay(key);
+        if (comp != 0) return null;
+
+
+        Node root = this.root;
+        final result = root;
+        final Node? left = root.left;
+        this.size--;
+
+
+        if (left == null) {
+            this.root = root.right;
+        } else {
+            final right = root.right;
+            root = this.splayMax(left);
+
+            root.right = right;
+            this.root = root;
+        }
+        this.modificationCount++;
+        return result;
+    }
+
+    Node addNewRoot(Node node, )
+}
